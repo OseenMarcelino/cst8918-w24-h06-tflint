0b4277b0a9cbd31e930cd758dfca58d9dec3f442
OseenM <oseenmarcelino.6@gmail.com>
2025-02-21 18:23:50 -0500

    Update: added description field to resource group

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 # Define output values for later reference
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
+  description = "This is the name of the resource group"
 }
 
 output "vm_name" {
